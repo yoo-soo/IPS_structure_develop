c3204aca0a748013376dbda6d9726242f9e445fc
SOOKYUNG YOO <80819675+yoo-soo@users.noreply.github.com>
2023-08-14 11:04:25 +0900

    Create IPS_queue.c

---
+++ IPS_queue.c
@@ -0,0 +1,41 @@
+#include "pcap_parser.h"
+  
+void initQueue(circular_queue *q) {
+  pthread_mutex_init(&(q->mutex), NULL);
+  q->front = 0;
+  q->rear = 0;
+}
+
+int isEmpty(circular_queue *q) {
+  return q->front == q->rear;
+}
+
+int isFull(circular_queue *q) {
+  return (q->front + 1) % QUEUE_MAX_SIZE == q->rear;
+}
+
+int enqueue_r(circular_queue *q, PKTDATA *pkdt) {
+  pthread_mutex_lock(&(q->mutex));
+  if (isFull(q)) {
+    //printf("Queue is full\n");
+    pthread_mutex_unlock(&(q->mutex));
+    return 0;
+  }
+  memcpy(&(q->items[q->front]), pkdt, sizeof(PKTDATA));
+  q->front = (q->front + 1) % QUEUE_MAX_SIZE;
+  pthread_mutex_unlock(&(q->mutex));
+  return 1;
+}
+
+int dequeue(circular_queue *q, PKTDATA *pkdt) {
+  pthread_mutex_lock(&(q->mutex));
+  if (isEmpty(q)) {
+    //printf("Queue is Empty\n");
+    pthread_mutex_unlock(&(q->mutex));
+    return 0;
+  }
+  memcpy(pkdt, &(q->items[q->rear]), sizeof(PKTDATA));
+  q->rear = (q->rear + 1) % QUEUE_MAX_SIZE;
+  pthread_mutex_unlock(&(q->mutex));
+  return 1;
+}
